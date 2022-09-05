bb94f0f478b78fdb1a94296f6c65fa19e5a181f2
raja <raja.rakoto7@gmail.com>
2022-09-05 11:11:54 +0300

    reorganize

---
+++ docs/applications.js
@@ -0,0 +1,40 @@
+export const applications = `
+---
+
+### \`📌 Applications\`
+
+Here are the lists of "applications" integrated in **sass-eo**
+
+##### \`⚫ GRID maker\`
+
+Generate basic CSS Grid code to make dynamic layouts !
+
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/sass-eo/grid-maker.gif?raw=true" width="500">
+</div>
+
+##### \`⚫ FLEXBOX maker\`
+
+Generate flexbox CSS code to make dynamic layouts !
+
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/sass-eo/flexbox-maker.gif?raw=true" width="500">
+</div>
+
+##### \`⚫ BUTTONS maker\`
+
+Generate stylish buttons in css !
+
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/sass-eo/buttons-maker.gif?raw=true" width="500">
+</div>
+
+##### \`⚫ BOX SHADOW maker\`
+
+Collection of shadow variants for your boxes !
+
+<div align="center">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/sass-eo/boxshadow-maker.gif?raw=true" width="500">
+</div>
+
+`
