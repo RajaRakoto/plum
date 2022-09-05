1deabe50d1995b79ae7769901a103ab1fb10f309
raja <raja.rakoto7@gmail.com>
2022-09-05 17:45:53 +0300

    reorganize

--- extension/src/sseo-basics.js
+++
@@ -1,7 +0,0 @@
-export const sseo_basics = {
-			"Import basics sass-eo module": {
-				"prefix": "sseo-import-basics",
-				"body": ["@import 'sass-eo-basics';"],
-				"description":  "Import basics sass-eo module"
-			}
-			};
---
+++ extension/src/sseo-import.js
@@ -0,0 +1,37 @@
+export const sseo_import = {
+	'Import basics sass-eo module': {
+		prefix: 'sseo-import-basics',
+		body: ["@import 'sass-eo-basics';"],
+		description: 'Import basics sass-eo module',
+	},
+
+	'Import milligrid (flexbox) module from sass-eo': {
+		prefix: 'sseo-import-milligrid',
+		body: ["@import 'sass-eo-milligrid';"],
+		description: 'Import milligrid (flexbox) module from sass-eo',
+	},
+
+	'Import semantic-ui (min) module from sass-eo': {
+		prefix: 'sseo-import-semantic',
+		body: ["@import 'sass-eo-semantic';"],
+		description: 'Import semantic-ui (min) module from sass-eo',
+	},
+
+	'Import bootstrap module (min) from sass-eo': {
+		prefix: 'sseo-import-bt',
+		body: ["@import 'sass-eo-bootstrap';"],
+		description: 'Import bootstrap module (min) from sass-eo',
+	},
+
+	'Import bootstrap (grid) module from sass-eo': {
+		prefix: 'sseo-import-bt-grid',
+		body: ["@import 'sass-eo-bootstrap-grid';"],
+		description: 'Import bootstrap (grid) module from sass-eo',
+	},
+
+	'Import bootstrap module (utils) from sass-eo': {
+		prefix: 'sseo-import-bt-utils',
+		body: ["@import 'sass-eo-bootstrap-utils';"],
+		description: 'Import bootstrap module (utils) from sass-eo',
+	},
+};
