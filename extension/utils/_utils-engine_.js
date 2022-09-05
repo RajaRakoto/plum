f20f08eea9e3e35e0465edd8bea5bf81c5a03c55
raja <raja.rakoto7@gmail.com>
2022-09-05 16:39:06 +0300

    upt engine

--- extension/utils/_utils-engine_.js
+++ extension/utils/_utils-engine_.js
@@ -1,5 +1,7 @@
 import traverse from 'traverse';
+import fs from 'fs';
 
+// to traverse all source object and get all values of the key
 export function traverseKeyValueByObject(source, key) {
 	let traverseResult = [];
 	traverse(source).forEach(function (e) {
@@ -8,4 +10,17 @@ export function traverseKeyValueByObject(source, key) {
 		}
 	});
 	return traverseResult;
+}
+
+// to read all source list
+export function fsReadDir(path) {
+	if (!fs.existsSync(path)){
+		fs.mkdirSync(path, { recursive: true });
+	}
+	return fs.readdirSync(path);
+}
+
+// to drop the last n characters of the string
+export function dropRight(arr, n = 1) {
+	return arr.slice(0, -n);
 }
