9f87a9b0566024e264ce3895d6ca12ea623f393d
raja <raja.rakoto7@gmail.com>
2022-09-05 17:57:59 +0300

    valid image snippets

--- extension/src/sseo-image.js
+++ extension/src/sseo-image.js
@@ -1,31 +1,47 @@
 export const sseo_image = {
-			"Image-logo mixin helps you create an SEO-optimized logo component for your brand's website using CSS best practices.": {
-				"prefix": "sseo-image-logo",
-				"body": ["@include image-logo($$image: ${1:url}, $$width: ${2:pixel}, $$height: ${3:pixel});"],
-				"description":  "Image-logo mixin helps you create an SEO-optimized logo component for your brand's website using CSS best practices."
-			},
-			
-			"Image-background mixin lets you apply background images to selected items.": {
-				"prefix": "sseo-image-background",
-				"body": ["@include image-background($$image: ${1:url}, $$filter-color: ${2:color list}, $$filter-direction: ${3:top, top-right, right, bottom-right, bottom, bottom-left, left, top-left});"],
-				"description":  "Image-background mixin lets you apply background images to selected items."
-			},
-			
-			"Add high-res images to your site, with a workaround for devices that don't display high-res images when using Retina Image, it's best to compress to the max without destroying image source (https://tinypng.com).": {
-				"prefix": "sseo-image-retina",
-				"body": ["@include image-retina($$image: ${1:url}, $$width: ${2:pixel}, $$height: ${3:pixel});"],
-				"description":  "Add high-res images to your site, with a workaround for devices that don't display high-res images when using Retina Image, it's best to compress to the max without destroying image source (https://tinypng.com)."
-			},
-			
-			"The image-resolution mixin helps you target elements to different screen resolutions.": {
-				"prefix": "sseo-image-resolution",
-				"body": ["@include image-resolution($$resolution: ${1:x1, x2, x3});"],
-				"description":  "The image-resolution mixin helps you target elements to different screen resolutions."
-			},
-			
-			"The image-responsive mixin helps you make images responsive and change the default display CSS property value from inline to block to remove the extra space below.": {
-				"prefix": "sseo-image-responsive",
-				"body": ["@include image-responsive($$height: ${1:pixel->auto});"],
-				"description":  "The image-responsive mixin helps you make images responsive and change the default display CSS property value from inline to block to remove the extra space below."
-			}
-			};
+	"[image]: Image-logo mixin helps you create an SEO-optimized logo component for your brand's website using CSS best practices.":
+		{
+			prefix: 'sseo-image-logo',
+			body: [
+				'@include image-logo($$image: ${1:url}, $$width: ${2:pixel}, $$height: ${3:pixel});',
+			],
+			description:
+				"[image]: Image-logo mixin helps you create an SEO-optimized logo component for your brand's website using CSS best practices.",
+		},
+
+	'[image]: Image-background mixin lets you apply background images to selected items.':
+		{
+			prefix: 'sseo-image-background',
+			body: [
+				'@include image-background($$image: ${1:url}, $$filter-color: ${2:color list}, $$filter-direction: ${3:top, top-right, right, bottom-right, bottom, bottom-left, left, top-left});',
+			],
+			description:
+				'[image]: Image-background mixin lets you apply background images to selected items.',
+		},
+
+	"[image]: Add high-res images to your site, with a workaround for devices that don't display high-res images when using Retina Image, it's best to compress to the max without destroying image source (https://tinypng.com).":
+		{
+			prefix: 'sseo-image-retina',
+			body: [
+				'@include image-retina($$image: ${1:url}, $$width: ${2:pixel}, $$height: ${3:pixel});',
+			],
+			description:
+				"[image]: Add high-res images to your site, with a workaround for devices that don't display high-res images when using Retina Image, it's best to compress to the max without destroying image source (https://tinypng.com).",
+		},
+
+	'[image]: The image-resolution mixin helps you target elements to different screen resolutions.':
+		{
+			prefix: 'sseo-image-resolution',
+			body: ['@include image-resolution($$resolution: ${1:x1, x2, x3});'],
+			description:
+				'[image]: The image-resolution mixin helps you target elements to different screen resolutions.',
+		},
+
+	'[image]: The image-responsive mixin helps you make images responsive and change the default display CSS property value from inline to block to remove the extra space below.':
+		{
+			prefix: 'sseo-image-responsive',
+			body: ['@include image-responsive($$height: ${1:pixel->auto});'],
+			description:
+				'[image]: The image-responsive mixin helps you make images responsive and change the default display CSS property value from inline to block to remove the extra space below.',
+		},
+};
