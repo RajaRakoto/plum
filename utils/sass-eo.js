fd6bae5be0476082bb4640e45e55c1dbd9a2539e
raja <raja.rakoto7@gmail.com>
2022-09-05 11:37:37 +0300

    utils: init sass-eo.js

---
+++ utils/sass-eo.js
@@ -0,0 +1,26 @@
+// docs importation
+import { applications } from '../docs/applications.js';
+import { banner } from '../docs/banner.js';
+import { basics } from '../docs/basics.js';
+import { desc } from '../docs/desc.js';
+import { extension } from '../docs/extension.js';
+import { frameworks } from '../docs/frameworks.js';
+import { install_update } from '../docs/install-update.js';
+import { libraries } from '../docs/libraries.js';
+import { usage } from '../docs/usage.js';
+
+console.log(banner)
+console.log(desc)
+console.log(install_update)
+console.log(usage)
+console.log(extension)
+console.log(basics)
+console.log(libraries)
+console.log(applications)
+console.log(frameworks)
+console.log(`
+<div align="center">
+
+#### Enjoy it 😉
+</div>
+`);
