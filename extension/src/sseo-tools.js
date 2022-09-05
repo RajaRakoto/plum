b388bd49b6c67cb479bb3734134653a004a2d1e6
raja <raja.rakoto7@gmail.com>
2022-09-05 18:08:22 +0300

    valid tools snippets

--- extension/src/sseo-tools.js
+++ extension/src/sseo-tools.js
@@ -1,7 +1,7 @@
 export const sseo_tools = {
-			"Generate image from internet using link": {
-				"prefix": "sseo-random-pic",
-				"body": ["https://picsum.photos/500/300?random=1"],
-				"description":  "Generate image from internet using link"
-			}
-			};
+	'[tools]: Generate image from internet using link': {
+		prefix: 'sseo-tools-randomPic',
+		body: ['https://picsum.photos/500/300?random=1'],
+		description: '[tools]: Generate image from internet using link',
+	},
+};
