2f33c308323bb4fdb991517121f1022eb4ffd6cf
raja <raja.rakoto7@gmail.com>
2022-09-05 17:03:33 +0300

    valid getES6moduleSyntaxBySource

--- extension/utils/es6.js
+++ extension/utils/es6.js
@@ -1,16 +1,20 @@
 // engine importation
-import { fsReadDir, dropRight } from './_utils-engine_.js';
+import { fsReadDir } from './_utils-engine_.js';
 
-// to get all ES6 modules from the source list
-function getES6syntax(source) {
+// to get all ES6 modules syntax from the source directory
+function getES6moduleSyntaxBySource(source, extension) {
+	const fixVarName = varName => varName.replace(/-/g, '_');
+	const dropRight = (arr, n = 1) => arr.slice(0, -n); 
 	let sourceList = fsReadDir(source);
 	let sourceES6 = [];
 	sourceList.forEach(source => {
-		if (source.endsWith('.js')) {
+		if (source.endsWith(extension)) {
 			sourceES6.push(dropRight(source, 3));
 		}
 	});
-	return sourceES6.map(source => `import { ${source} } from './${source}.js';`);
+	return sourceES6.map(
+		source => `import { ${fixVarName(source)} } from './${source}.js';`,
+	);
 }
 
-console.log(getES6syntax('../src'));
+console.log(getES6moduleSyntaxBySource('../src', '.js'));
