d82bc2e281f32c96e199e8f4787481133f6b238b
raja <raja.rakoto7@gmail.com>
2024-03-08 16:42:13 +0300

    scripts: set global docs order

--- scripts/global-docs.js
+++ scripts/global-docs.js
@@ -2,30 +2,16 @@
  * This script is used to generate the documentation for the project.
  */
 
-import { applications } from '../docs/applications.js'
 import { banner } from '../docs/banner.js'
 import { basics } from '../docs/basics.js'
-import { desc } from '../docs/desc.js'
 import { extension } from '../docs/extension.js'
-import { frameworks } from '../docs/frameworks.js'
 import { get_started } from '../docs/get-started.js'
-import { libraries } from '../docs/libraries.js'
-import { usage } from '../docs/usage.js'
-import { contrib } from '../docs/contrib.js'
+import { modules_apps_scripts } from '../docs/modules-apps-scripts.js'
+import { npm_scripts } from '../docs/npm_scripts.js'
 
 console.log(banner)
-console.log(desc)
 console.log(get_started)
-console.log(contrib)
-console.log(usage)
-console.log(extension)
 console.log(basics)
-console.log(libraries)
-console.log(applications)
-console.log(frameworks)
-console.log(`
-<div align="center">
-
-#### Enjoy it 😉
-</div>
-`)
+console.log(extension)
+console.log(modules_apps_scripts)
+console.log(npm_scripts)
