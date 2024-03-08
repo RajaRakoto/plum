7b6b265d2856cb447f32cc234ac91e312002ed7b
raja <raja.rakoto7@gmail.com>
2024-03-08 16:51:58 +0300

    update docs

--- docs/get-started.js
+++ docs/get-started.js
@@ -15,5 +15,5 @@ Execute the following command to start CLI:
 plum-cli
 \`\`\`
 
-<img src="https://github.com/RajaRakoto/github-docs/blob/master/plum/plum-cli-demo.gif?raw=true" width="500">
+<img src="https://github.com/RajaRakoto/github-docs/blob/master/plum/plum-cli-demo.gif?raw=true" width="800">
 `
